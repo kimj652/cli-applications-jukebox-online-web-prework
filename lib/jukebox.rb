d954890929ddafa9e80762efe0b2df6de5f42664
J K <kim.ju652@gmail.com>
2019-08-05 03:43:23 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -16,6 +16,7 @@ def help
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
+  
 end
 
 def list
